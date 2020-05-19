6c07fbf462136069d3303ca45f7a76546d754a4c
Luana Andrade <luana.las21@gmail.com>
2020-05-19 13:37:15 -0300

    Updates

---
+++ data.js
@@ -0,0 +1,51 @@
+import data from "./data/lol/lol.js";
+const lol = data.data;
+console.log(lol);
+
+export const filterBy = (lolObject, searchdTerm) => {
+  const lolArray = Object.values(lolObject);
+
+  return lolArray.filter((champion) => {
+    return champion.name.toUpperCase().indexOf(searchdTerm.toUpperCase()) === 0;
+  });
+};
+
+export const orderBy = (lolObject, param, ascending) => {
+  const lolArray = Object.values(lolObject);
+
+  return lolArray.sort((championA, championB) => {
+    let a = "";
+    let b = "";
+    if (param === "armor") {
+      a = championA.stats.armor;
+      b = championB.stats.armor;
+    } else if (param === "hp") {
+      a = championA.stats.hp;
+      b = championB.stats.hp;
+    } else if (param === "mp") {
+      a = championA.stats.mp;
+      b = championB.stats.mp;
+    } else if (param === "name") {
+      a = championA.lol.name;
+      b = championB.lol.name;
+    }
+
+    const ascendingOrder = a < b ? 1 : -1;
+    const descendingOrder = ascendingOrder * -1;
+
+    return ascending ? ascendingOrder : descendingOrder;
+  });
+};
+
+export const calculated = (championA, championB) => {
+  let lowestMp = championA.stats.armor;
+
+  let HigherMp = championB.stats.armor;
+
+  let porcentage = ((lowestMp - HigherMp) * 100) / lowestMp;
+};
+console.log(calculated);
+
+
+
+  
---
+++ main.js
@@ -0,0 +1,63 @@
+import data from "./data/lol/lol.js";
+import { filterBy } from "./data.js";
+import { orderBy } from "./data.js";
+
+const lol = data.data;
+
+const lolList = document.getElementById("lol-list");
+
+const renderLol = (lol) => {
+  const template = document.getElementById("lol-template");
+  const card = template.content.querySelector(".card");
+  const championsName = card.querySelector(".name");
+  const championsImage = card.querySelector(".img");
+  const championsArmor = card.querySelector(".armor");
+  const championsMP = card.querySelector(".MP");
+  const championsHP = card.querySelector(".HP");
+
+  championsName.textContent = lol.name;
+  championsArmor.textContent = "Armor:" + lol.stats.armor;
+  championsMP.textContent = "MP:" + lol.stats.mp;
+  championsHP.textContent = "HP:" + lol.stats.hp;
+  championsImage.setAttribute("src", lol.img);
+  const clonedTemplate = document.importNode(template.content, true);
+  lolList.appendChild(clonedTemplate);
+};
+
+const renderChampions = (lolData) => {
+  const lolArray = Object.values(lolData);
+  lolArray.forEach(renderLol);
+};
+renderChampions(lol);
+
+const filterByNameInput = document.getElementById("filter-by-name");
+
+const renderSearchByName = () => {
+  const search = filterByNameInput.value;
+  const result = filterBy(lol, search);
+  lolList.innerHTML = "";
+  renderChampions(result);
+};
+
+const sortInput = document.getElementById("sort");
+
+const renderOrderBy = () => {
+  const searchValue = sortInput.value;
+  const [order, param] = searchValue.split("-");
+  const isAscending = order === "ascending";
+  const result = orderBy(lol, param, isAscending);
+
+  lolList.innerHTML = "";
+  renderChampions(result);
+};
+
+sortInput.addEventListener("change", renderOrderBy);
+filterByNameInput.addEventListener("keyup", renderSearchByName);
+
+
+
+
+
+
+
+  
